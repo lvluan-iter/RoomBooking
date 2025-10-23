4374532ae845ec18a5e6fd7d2b3c9397ad97d710
31211027594 <luanle.31211027594@st.ueh.edu.vn>
2025-10-23 17:12:48 +0700

    Add BE deploy domain for health check

--- src/main/java/com/example/RoomBooking/controllers/HealthCheckController.java
+++ src/main/java/com/example/RoomBooking/controllers/HealthCheckController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/health")
+@RequestMapping("/api")
 public class HealthCheckController {
 
-    @GetMapping
+    @GetMapping("/health")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("OK");
     }
