84a7225f4d811b3200e7f525b08793ca6c47e0fc
Luan Le <142068838+lvluan-iter@users.noreply.github.com>
2025-10-25 12:10:13 +0700

    Modify ApiResponseWrapper to always support response types

--- src/main/java/com/example/RoomBooking/config/ApiResponseWrapper.java
+++ src/main/java/com/example/RoomBooking/config/ApiResponseWrapper.java
@@ -8,14 +8,14 @@ import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
+@RestControllerAdvice
 public class ApiResponseWrapper implements ResponseBodyAdvice<Object> {
-    
+
     @Override
     public boolean supports(MethodParameter returnType, Class converterType) {
-        String converterName = converterType.getSimpleName();
-        return !"StringHttpMessageConverter".equals(converterName);
+        return true;
     }
-    
+
     @Override
     public Object beforeBodyWrite(Object body,
                                   MethodParameter returnType,
@@ -23,11 +23,11 @@ public class ApiResponseWrapper implements ResponseBodyAdvice<Object> {
                                   Class converterType,
                                   ServerHttpRequest request,
                                   ServerHttpResponse response) {
-        
+
         if (body instanceof ApiResult) {
             return body;
         }
-        
+
         return ApiResult.success(body);
     }
 }
